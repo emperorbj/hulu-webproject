e6ed93e6016143b336547d68f6efd983cebc45a4
emperorbj <138551605+emperorbj@users.noreply.github.com>
2023-12-24 16:50:49 -0800

    adding pop up modal window functionality

--- hulu/scripts/practise.js
+++ hulu/scripts/practise.js
@@ -9,29 +9,29 @@ document.querySelector('.nav--links').addEventListener('click', function(e){
  })
 
 // MODAL WINDOW ACTIONS
-// const openBtn = document.querySelectorAll('.btn--show--modal')
-// const closeBtn = document.querySelector('.btn--close--modal')
-// const modal = document.querySelector('.modal')
-// const overlay = document.querySelector('.overlay')
+ const openBtn = document.querySelectorAll('.btn--show--modal')
+ const closeBtn = document.querySelector('.btn--close--modal')
+ const modal = document.querySelector('.modal')
+ const overlay = document.querySelector('.overlay')
 
 
-// closeBtn.addEventListener('click',closeModal)
+ closeBtn.addEventListener('click',closeModal)
 
-// function openModal(){
-//     modal.classList.remove('hidden')
-//     overlay.classList.remove('hidden')
-// }
+ function openModal(){
+     modal.classList.remove('hidden')
+     overlay.classList.remove('hidden')
+ }
 
-// for (let i = 0; i < openBtn.length; i++)
-// openBtn[i].addEventListener('click', openModal);
+ for (let i = 0; i < openBtn.length; i++)
+ openBtn[i].addEventListener('click', openModal);
 
 //OR WE CAN LOOP USING FOR EACH
 // openBtn.forEach( btn => {
 //     btn.addEventListener('click',openModal)
 // })
 
-// function closeModal(){
-//     modal.classList.add('hidden')
-//     overlay.classList.add('hidden')
-// }
+ function closeModal(){
+     modal.classList.add('hidden')
+     overlay.classList.add('hidden')
+ }
 
